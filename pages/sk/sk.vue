1768aed734eef8e3b8e4e45b0765b7effcb46d96
liuhao <liuhaos2000@163.com>
2025-11-10 19:50:25 +0800

    nav

--- pages/sk/sk.vue
+++ pages/sk/sk.vue
@@ -41,6 +41,11 @@
 
 		</view>
 	</view>
+	
+	<vive>
+		<uni-goods-nav :fill="true"  :options="options" :buttonGroup="buttonGroup"  @click="onClick" @buttonClick="buttonClick" />
+	</vive>
+
 </template>
 
 <script>
@@ -169,8 +174,49 @@ export default {
 
 	// keep uni-app onLoad lifecycle to receive navigation params
 	async onLoad(option) {
+	},
+	
+	data () {
+	  return {
+	    options: [{
+			icon: 'headphones',
+			text: '客服'
+		}, {
+			icon: 'shop',
+			text: '店铺',
+			info: 2,
+			infoBackgroundColor:'#007aff',
+			infoColor:"red"
+		}, {
+			icon: 'cart',
+			text: '购物车',
+			info: 2
+		}],
+	    buttonGroup: [{
+	      text: '加入购物车',
+	      backgroundColor: '#ff0000',
+	      color: '#fff'
+	    },
+	    {
+	      text: '立即购买',
+	      backgroundColor: '#ffa200',
+	      color: '#fff'
+	    }
+	    ]
+	  }
+	},
+	methods: {
+	  onClick (e) {
+	    uni.showToast({
+	      title: `点击${e.content.text}`,
+	      icon: 'none'
+	    })
+	  },
+	  buttonClick (e) {
+	    console.log(e)
+	    this.options[2].info++
+	  }
 	}
-
 
 }
 </script>
